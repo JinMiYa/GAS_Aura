d99f93237d4f4aece8012a7da1be8f0ab7c25294
Jinmy <1005461445@qq.com>
2024-04-16 23:11:38 +0800

    正常备份 20240416

---
+++ Source/Aura_GAS/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,41 @@
+// Copyright JINMY
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+struct FWidgetControllerParams;
+class UOverlayWidgetController;
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class AURA_GAS_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+
+	void InitOverlay(APlayerController* PC,APlayerState* PS,UAbilitySystemComponent* ASC,UAttributeSet* AS);
+	
+protected:
+	
+private:
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UOverlayWidgetController> OverlayWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+};
